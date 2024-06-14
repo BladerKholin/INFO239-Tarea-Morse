bd0e0c5c8647f167e63b3cfff9b8747f9849901a
yiyoZX <105678117+yiyoZX@users.noreply.github.com>
2024-06-14 15:12:44 -0400

    Update Maestro.ino

--- Maestro/Maestro.ino
+++ Maestro/Maestro.ino
@@ -2,6 +2,7 @@
 
 const int buttonPin = 9; // Pin donde está conectado el botón
 int buttonState = 0;     // Variable para almacenar el estado del botón
+char c;
 
 void setup() {
   Wire.begin();           // Iniciar como maestro
@@ -11,10 +12,21 @@ void setup() {
 
 void loop() {
   buttonState = digitalRead(buttonPin); // Leer el estado del botón
-  Serial.println(buttonState); // Imprimir el estado del botón para depuración
+  
   Wire.beginTransmission(8); // Iniciar transmisión al esclavo con dirección 8
   Wire.write(buttonState);   // Enviar el estado del botón
   Wire.endTransmission();    // Finalizar transmisión
+  
+  Wire.requestFrom(8, 20); // Solicita hasta 20 bytes del esclavo con dirección 8
+  if( (Wire.available()) and (c = Wire.read())!=" ") {
+    //c = Wire.read(); // Lee cada byte enviado por el esclavo
+    receivedString += c; // Añade el byte al string
+  }
+  else if( (Wire.available()) and (c = Wire.read())==" ") {
+    //c = Wire.read(); // Lee cada byte enviado por el esclavo
+    
+    Serial.print(receiveString);
+  }
 
- 
+  Serial.println(buttonState); // Imprimir el estado del botón para depuración
 }
