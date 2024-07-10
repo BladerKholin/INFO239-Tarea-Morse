deb73c318ea42e972f248fabf0a2b6416f01d777
cristobal-rebolledo <114099837+cristobal-rebolledo@users.noreply.github.com>
2024-07-09 23:57:16 -0400

    transmisor con mensaje  de entrada por termina

--- Parte 2/transmisor/transmisor_v2.ino
+++ Parte 2/transmisor/transmisor_v2.ino
@@ -1,39 +1,79 @@
+/*
+ver ::cl 20120520
+Configuracion basica para modulo transmisor RT 11
+Utiliza libreria VirtualWire.h
+pin 01 entrada desde Arduino pin digital 2
+pin 02 Tierra
+pin 07 tierra
+pin 08 antena externa
+pin 09 tierra
+pin 10 5v
+*/
+
 #include <VirtualWire.h>
 
 
-String msg = "holachao?juan";
+//String msg = "holachao?juan";
+String msg = "";
+String destino = "";
+bool stringComplete = false;
 int num_paquete = 1;
 
 struct Packet {
-    unsigned short origen = 0x08; // 2 bytes
-    unsigned short destino = 0x04; // 2 bytes
+    unsigned short origen = 10; // 2 bytes
+    unsigned short destino; // 2 bytes
     unsigned short crc = 0x00; // 2 bytes
     unsigned char secuencia; // 1 byte
     unsigned char total;  // 1 byte
     char message[8];  
 };
 
+
+char paquetesTotales(){
+  int total = (((int)msg.length()-1) / 8);
+    if((msg.length()-1) % 8 == 0){
+      if(msg.length()-1 == 8){
+        num_paquete = 1;
+      }
+      return (char)total;
+    }
+    else
+      return (char)(total +1);
+}
+
+
 void creaPaquete(String msg2){
     Packet packet;
     strncpy(packet.message, msg2.c_str(), sizeof(packet.message) );
     packet.secuencia = (char)(num_paquete++);
     
-    
-    int total = (((int)msg.length()) / 8);
-    if(msg.length() == 8){
-      num_paquete = 1;
-      packet.total = (char)total;
-    }
-      
-    else
-      packet.total = (char)(total +1);
+    packet.total = paquetesTotales();
 
-
+    packet.destino = (unsigned short)(destino.toInt());
     // ENvia paquete
     char charArray[sizeof(Packet)];
     memcpy(charArray, &packet, sizeof(Packet));
-    Serial.println(msg2);  
+    for(int i = 0;i < 8;i++)
+      Serial.print(packet.message[i]);
+    Serial.println();  
     vw_send((uint8_t *)charArray, sizeof(charArray)); 
+}
+
+String ingresaTexto(const char* prompt) {
+  String texto = "";
+  Serial.println(prompt);
+  while (!stringComplete) {
+    while (Serial.available() > 0) {
+      char receivedChar = (char)Serial.read();
+      texto += receivedChar;
+
+      if (receivedChar == '\n') {
+        stringComplete = true;
+      }
+    }
+  }
+  stringComplete = false;
+  return texto;
 }
 
 void setup(){
@@ -41,30 +81,39 @@ void setup(){
     vw_setup(2000);
     vw_set_tx_pin(2);    
     Serial.begin(9600);
+   
+    
+
 }
 
-void loop(){
-    
+void loop() {
+  if (destino.length() == 0) {
+    destino = ingresaTexto("# Ingrese destino: ");
+    Serial.print("Destino: " + destino + "\n");
+  }
+
+  if (msg.length() == 0 && destino.length() > 0) {
+    msg = ingresaTexto("# Ingrese mensaje:");
+  }
+
+  if (msg.length() > 0 && destino.length() > 0) {
     String msg2 = "";
-      for(int i = 0; i < msg.length(); i++){
-          msg2 += msg[i]; 
-          if(msg2.length() == 8){
-              creaPaquete(msg2);
-              msg2 = "";
-            
-          }
-          
-          else if((msg.length() - i) <= 7 && i == msg.length() -1){
-            
-              creaPaquete(msg2);
-              msg2 = "";
-              num_paquete = 1;
-          }
+    for (int i = 0; i < msg.length(); i++) {
+      msg2 += msg[i];
+      if (msg2.length() == 8) {
+        creaPaquete(msg2);
+        msg2 = "";
+      } else if ((msg.length() - i) <= 7 && i == msg.length() - 1) {
+        creaPaquete(msg2);
+        msg2 = "";
+        num_paquete = 1; 
       }
+    }
+    Serial.print("Mensaje con destino a arduino " + destino + " ¡ Ha sido enviado!\n\n");
+    msg = ""; 
+    destino = ""; 
 
-    
-    
-    Serial.println("Enviado!");
-    vw_wait_tx();
-    delay(5000);
+  delay(1000);
 }
+}
+
