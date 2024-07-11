46af82b7e6f3fc0f126a521a07fec2545d38fdf8
cristobal-rebolledo <114099837+cristobal-rebolledo@users.noreply.github.com>
2024-07-10 21:04:15 -0400

    Arreglar: guardar el origen del msg para que se pueda utilizar en el último IF

--- Parte 2/receptor/receptor_v4.ino
+++ Parte 2/receptor/receptor_v4.ino
@@ -1,70 +1,116 @@
 #include <VirtualWire.h>
 
+
+struct Tuple {
+    unsigned long tiempo_inicio;
+    unsigned long tiempo_fin;
+};
+
 unsigned short id_arduino = 0x04;
-unsigned short broadcast = 0x00; 
+unsigned short broadcast = 0x00;
 String msg = "";
 int num_secuencia = 0;
+int tiempo_paquete = 500;
+bool grupos[10] = {false,false,false,false,false,false,false,false,false,false};
+Tuple tiempos[10]; 
+
 struct Packet {
     unsigned short origen; // 2 bytes
     unsigned short destino; // 2 bytes
     unsigned short crc; // 2 bytes
     unsigned char secuencia; // 1 byte
     unsigned char total;  // 1 byte
-    char message[8];  
+    char message[8];
 };
-void setup(){
+
+void setup() {
     Serial.begin(9600);
     Serial.println("Configurando Recepcion");
-    vw_set_ptt_inverted(true); 
+    vw_set_ptt_inverted(true);
     vw_setup(2000);
     vw_set_rx_pin(2);
     vw_rx_start();
 }
 
-void loop(){
+void loop() {
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-    if (vw_get_message(buf, &buflen)){
-        ++num_secuencia;
+    unsigned long tiempo_recepcion = millis();
+    if (vw_get_message(buf, &buflen)) {
+        num_secuencia++;
         Packet packet;
-        if (buflen == sizeof(Packet)) {  
+        //if (buflen == sizeof(Packet)) {
             memcpy(&packet, buf, sizeof(Packet));
-          
-            if(packet.destino == id_arduino || packet.destino == broadcast){
-                Serial.print("Origen: ");
-                Serial.println(packet.origen);
-           
-                
-                Serial.print("Total : ");
-                Serial.println(packet.total);
-                for(int i = 0; i < 8;i++){
-                  msg += packet.message[i];
-                }
-                
-                if(num_secuencia == packet.secuencia){
+            //int tiempo_mensaje = tiempo_paquete*packet.total;
+            if (packet.destino == id_arduino || packet.destino == broadcast) { 
+
+                if( grupos[packet.origen] == false){
+                    Serial.print("no está\n");
+                    grupos[packet.origen] = true;
+                    tiempos[packet.origen].tiempo_inicio = millis();
+                    tiempos[packet.origen].tiempo_fin = millis() + (tiempo_paquete * packet.total);
+                    
+                    Serial.print("Origen: ");
+                    Serial.println(packet.origen);
+
+                    Serial.print("Total : ");
+                    Serial.println(packet.total);  
+
+                    for (int i = 0; i < 8; i++) {
+                        msg += packet.message[i];
+                    }
+
+                    Serial.print("Mensaje primer paquete: ");
+                    Serial.println(msg);   
+
                     Serial.print("Secuencia: ");
-                    Serial.println(packet.secuencia);
+                    Serial.println(packet.secuencia);     
+                    Serial.print("tiempo llego: " + String(tiempos[packet.origen].tiempo_inicio) + " tiempo fin " + String(tiempos[packet.origen].tiempo_fin) + " \n" );     
                 }
                 else{
-                  Serial.println("Paquete perdido");
-                   msg += "##"+ String(num_secuencia) +"##";
-                   Serial.println(num_secuencia);
+                  for (int i = 0; i < 8; i++) {
+                        msg += packet.message[i];
+                    }
+
+                    Serial.print("Origen: ");
+                    Serial.println(packet.origen);
+
+                    Serial.print("Total : ");
+                    Serial.println(packet.total);  
+
+                    Serial.print("Secuencia: ");
+                    Serial.println(packet.secuencia);
+
                 } 
-
-
                 
-                Serial.println("Progreso del mensaje:");
-                Serial.println(msg);
-                if(num_secuencia == packet.total){
-                  Serial.println("Mensaje llegó al final");
-                  num_secuencia = 0;
-                  msg = "";
+                
+                if(num_secuencia != packet.secuencia) {
+                    Serial.println("Paquete perdido");
+                    Serial.println(num_secuencia);
+                    // Añadir marcador de paquete perdido y reiniciar secuencia
+                    msg += "##" + String(num_secuencia) + "##";
                 }
-            }
-            
-        } else {
-            Serial.println("Error: Tamaño del paquete incorrecto");
-        }
+
+                if(num_secuencia == packet.total ){
+                  Serial.print("Mensaje completo: ");
+                  Serial.println(msg);
+                  msg = "";
+                  grupos[packet.origen] = false;
+
+                  Serial.print("Cantidad de paquetes: " + String(num_secuencia) + " \n");
+                  num_secuencia = 0;
+                  
+                }
+
+                }
+                
+    }
+    if(grupos[4] == true && tiempos[4].tiempo_fin   == (unsigned long)millis()){
+      Serial.print("Mensaje por tiempo: " + msg + "\n");
+      Serial.println(millis());
+      msg = "";
+      grupos[4] = false;
+      Serial.print("Cantidad de paquetes: " + String(num_secuencia) + " \n");
+      num_secuencia = 0;
     }
 }
